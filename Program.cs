09f7acc946a593460835eec4856ab8a7ef0f1894
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-03 20:07:37 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Forms.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMudServices();
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
