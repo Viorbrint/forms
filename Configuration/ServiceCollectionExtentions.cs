48dcb84ea5e312a3ae3ad1090e8d156d3a44ab78
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-07 16:14:12 +0300

    refactor: split Program.cs

---
+++ Configuration/ServiceCollectionExtentions.cs
@@ -0,0 +1,60 @@
+using Forms.Data;
+using Forms.Data.Entities;
+using Forms.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forms.Configuration;
+
+public static class ServiceCollectionExtentions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IAuthService, IdentityAuthService>();
+        return services;
+    }
+
+    public static IServiceCollection ConfigureDbContext(this IServiceCollection services)
+    {
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        if (string.IsNullOrEmpty(databaseUrl))
+        {
+            throw new InvalidOperationException("DATABASE_URL environment variable is not set.");
+        }
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(databaseUrl));
+        return services;
+    }
+
+    public static IServiceCollection AddIdentityConfig(this IServiceCollection services)
+    {
+        services
+            .AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.SignIn.RequireConfirmedAccount = false;
+
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        return services;
+    }
+
+    public static IServiceCollection AddCookieConfig(this IServiceCollection services)
+    {
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromHours(1);
+            options.SlidingExpiration = true;
+        });
+        return services;
+    }
+}
