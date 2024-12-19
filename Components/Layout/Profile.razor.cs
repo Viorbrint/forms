5a0b0e1bb33a04c50b13b25ee5ad5130b4056cc5
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-19 21:51:11 +0300

    fix: Profile.razor.cs

--- Components/Layout/Profile.razor.cs
+++ Components/Layout/Profile.razor.cs
@@ -1,27 +1,29 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Forms.Components.Layout;
 
 public partial class Profile : ComponentBase
 {
-    [Inject] private IHttpContextAccessor HttpContextAccessor { get; set; } = null!;
-    
+    [Inject]
+    private IHttpContextAccessor HttpContextAccessor { get; set; } = null!;
+
     [Inject]
     NavigationManager NavigationManager { get; set; } = null!;
 
     private string? _letter;
+
     private void Logout()
     {
         NavigationManager.NavigateTo("/logout", forceLoad: true);
     }
-    
-      protected override void OnInitialized()
-      {
-          var context = HttpContextAccessor.HttpContext;
-          if (context is { User.Identity.Name: not null })
-          {
-              _letter = context.User.Identity.Name[0].ToString();
-          }
-      }
+
+    protected override void OnInitialized()
+    {
+        var context = HttpContextAccessor.HttpContext;
+        if (context is { User.Identity.Name: not null })
+        {
+            _letter = context.User.Identity.Name[0].ToString();
+        }
+    }
 }
+
