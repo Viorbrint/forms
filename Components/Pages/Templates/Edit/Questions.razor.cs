035043d7aca8d43d94b320f59d6eb84ef7227530
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-19 21:52:00 +0300

    fix: Questions.razor.cs

--- Components/Pages/Templates/Edit/Questions.razor.cs
+++ Components/Pages/Templates/Edit/Questions.razor.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using MudBlazor.Interfaces;
 
 namespace Forms.Components.Pages.Templates.Edit;
 
 public partial class Questions : ComponentBase
 {
     private MudDropContainer<DropItem> _dropContainer = null!;
+
     private void ItemUpdated(MudItemDropInfo<DropItem> dropItem)
     {
         Console.WriteLine(dropItem.IndexInZone);
@@ -18,20 +18,18 @@ public partial class Questions : ComponentBase
     {
         if (_dropContainer == null)
         {
-            Console.WriteLine("null");
+            return;
         }
-        _items.Add(new DropItem(){ Name = "Item 2", Selector = $"{++_i}" });
+        _items.Add(new DropItem() { Name = "Item 2", Selector = $"{++_i}" });
         _dropContainer.Refresh();
     }
 
-    private List<DropItem> _items =
-    [
-        new DropItem() { Name = "Item 1", Selector = $"1" },
-    ];
-    
+    private List<DropItem> _items = [new DropItem() { Name = "Item 1", Selector = $"1" }];
+
     public class DropItem
     {
-        public string Name { get; init; }
-        public string Selector { get; set; }
+        public string Name { get; init; } = string.Empty;
+        public string Selector { get; set; } = string.Empty;
     }
 }
+
