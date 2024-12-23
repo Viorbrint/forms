9605aa5f57166bda91be6f056f219e442d85b02f
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-23 23:10:27 +0300

    feat: protect template edit page

--- Components/Pages/Templates/Edit/TemplatesEdit.razor.cs
+++ Components/Pages/Templates/Edit/TemplatesEdit.razor.cs
@@ -1,3 +1,4 @@
+using Forms.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace Forms.Components.Pages.Templates.Edit;
@@ -5,5 +6,32 @@ namespace Forms.Components.Pages.Templates.Edit;
 public partial class TemplatesEdit : ComponentBase
 {
     [Parameter]
-    public string? TemplateId { get; set; }
+    public string TemplateId { get; set; } = null!;
+
+    [Inject]
+    private ICurrentUserService CurrentUserService { get; set; } = null!;
+
+    [Inject]
+    private TemplateService TemplateService { get; set; } = null!;
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var template = await TemplateService.GetByIdAsync(TemplateId);
+        if (template == null)
+        {
+            NavigationManager.NavigateTo("/notfound");
+            return;
+        }
+
+        var userId = CurrentUserService.GetUserId();
+        var isAdmin = CurrentUserService.IsAdmin();
+        if (!isAdmin && template.AuthorId != userId)
+        {
+            NavigationManager.NavigateTo("/accessdenied");
+            return;
+        }
+    }
 }
