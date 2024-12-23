17d48aeba2598da47d153b7d572de0f8d4290162
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-23 23:11:15 +0300

    feat: improve template management

--- Components/Pages/UserPage/UserTemplates.razor.cs
+++ Components/Pages/UserPage/UserTemplates.razor.cs
@@ -1,5 +1,7 @@
 using Forms.Data.Entities;
+using Forms.Services;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Forms.Components.Pages.UserPage;
 
@@ -9,22 +11,61 @@ public partial class UserTemplates : ComponentBase
 
     private HashSet<Template> SelectedTemplates { get; set; } = [];
 
-    private async Task DeleteTemplate()
+    [Inject]
+    private TemplateService TemplateService { get; set; } = null!;
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    private ICurrentUserService CurrentUserService { get; set; } = null!;
+
+    private string userId = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        userId = CurrentUserService.GetUserId()!;
+        Templates = await TemplateService.GetByUserAsync(userId);
+    }
+
+    private async Task DeleteTemplates()
     {
         await Task.CompletedTask;
         throw new NotImplementedException();
     }
 
-    private async Task EditTemplate()
+    private void OnTemplateClick(TableRowClickEventArgs<Template> args)
     {
-        await Task.CompletedTask;
-        throw new NotImplementedException();
+        var template = args.Item;
+        if (template != null)
+        {
+            NavigateToEdit(template.Id);
+        }
+    }
+
+    private void NavigateToEdit(string templateId)
+    {
+        var editPath = $"/templates/edit/{templateId}";
+        NavigationManager.NavigateTo(editPath);
+    }
+
+    private void EditSelected()
+    {
+        if (SelectedTemplates.Count() != 1)
+        {
+            return;
+        }
+        var template = SelectedTemplates.First();
+        NavigateToEdit(template.Id);
     }
 
     private async Task CreateTemplate()
     {
-        await Task.CompletedTask;
-        throw new NotImplementedException();
+        // TODO: move this
+        const string DEFAULT_TOPIC_ID = "20";
+        Template template = new() { AuthorId = userId, TopicId = DEFAULT_TOPIC_ID };
+        await TemplateService.AddAsync(template);
+
+        NavigateToEdit(template.Id);
     }
 }
-
