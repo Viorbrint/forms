4e07e32f7b3f2b121d356486f65515f7b123ebf0
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-23 23:04:20 +0300

    refactor: clean user page logic

--- Components/Pages/UserPage/UserPage.razor.cs
+++ Components/Pages/UserPage/UserPage.razor.cs
@@ -1,36 +1,5 @@
-using Forms.Data;
-using Forms.Data.Entities;
 using Microsoft.AspNetCore.Components;
-using Microsoft.EntityFrameworkCore;
 
 namespace Forms.Components.Pages.UserPage;
 
-public partial class UserPage : ComponentBase
-{
-    [Inject]
-    private IHttpContextAccessor HttpContextAccessor { get; set; } = null!;
-
-    // TODO: not good
-    [Inject]
-    private ApplicationDbContext Db { get; set; } = null!;
-
-    private User CurrentUser { get; set; } = null!;
-
-    protected override void OnInitialized()
-    {
-        var context = HttpContextAccessor.HttpContext;
-        if (context is not { User.Identity.Name: not null })
-        {
-            return;
-        }
-
-        var user = Db
-            .Users.Include(u => u.Templates)
-            .FirstOrDefault(u => u.UserName == context.User.Identity.Name);
-        if (user == null)
-        {
-            return;
-        }
-        CurrentUser = user;
-    }
-}
+public partial class UserPage : ComponentBase { }
