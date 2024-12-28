d35a260f060b726cd719bfa35fed38fff5db78e1
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-28 21:37:44 +0300

    fix: add text to comments in db

---
+++ Migrations/20241228183653_TextToComment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forms.Migrations
+{
+    /// <inheritdoc />
+    public partial class TextToComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CommentText",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentText",
+                table: "Comments");
+        }
+    }
+}
