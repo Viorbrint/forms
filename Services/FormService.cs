e3f3ae8968e9c4fb94ff211bbd66ddfff111f996
Bogdan Petrukovich <viorbrint@gmail.com>
2024-12-31 18:18:11 +0300

    feat: implement get by id

--- Services/FormService.cs
+++ Services/FormService.cs
@@ -21,4 +21,20 @@ public class FormService(IRepository<Form> formRepository)
         var spec = new Specification<Form>(t => ids.Contains(t.Id));
         await formRepository.DeleteBySpecificationAsync(spec);
     }
+
+    public async Task<Form?> GetByIdAsync(string formId)
+    {
+        var spec = new SpecificationSingle<Form>(t => t.Id == formId);
+        spec.AddInclude(f => f.SingleLineAnswers);
+        spec.AddInclude(f => f.MultiLineAnswers);
+        spec.AddInclude(f => f.BooleanAnswers);
+        spec.AddInclude(f => f.NumberAnswers);
+        var result = await formRepository.GetBySpecificationSingleAsync(spec);
+        return result;
+    }
+
+    public async Task UpdateAsync(Form form)
+    {
+        await formRepository.UpdateAsync(form);
+    }
 }
