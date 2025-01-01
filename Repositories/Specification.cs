44cfcca30da67cfc8fd3e3b341e6b972dbb2bc4e
Bogdan Petrukovich <viorbrint@gmail.com>
2025-01-01 19:42:00 +0300

    feat: add paging ability to repository

--- Repositories/Specification.cs
+++ Repositories/Specification.cs
@@ -2,25 +2,29 @@ using System.Linq.Expressions;
 
 namespace Forms.Repositories;
 
-public class Specification<T> : ISpecification<T>
+public class Specification<T>(
+    Expression<Func<T, bool>>? criteria = null,
+    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null
+) : ISpecification<T>
 {
-    public Expression<Func<T, bool>>? Criteria { get; }
+    public Expression<Func<T, bool>>? Criteria { get; } = criteria;
 
-    public Func<IQueryable<T>, IOrderedQueryable<T>>? OrderBy { get; }
+    public Func<IQueryable<T>, IOrderedQueryable<T>>? OrderBy { get; } = orderBy;
 
     public List<Expression<Func<T, object>>> Includes { get; } = [];
+
+    public int? Take { get; private set; }
+
+    public int? Skip { get; private set; }
+
+    public void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+    }
 
     public void AddInclude(Expression<Func<T, object>> includeExpression)
     {
         Includes.Add(includeExpression);
-    }
-
-    public Specification(
-        Expression<Func<T, bool>>? criteria = null,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null
-    )
-    {
-        Criteria = criteria;
-        OrderBy = orderBy;
     }
 }
