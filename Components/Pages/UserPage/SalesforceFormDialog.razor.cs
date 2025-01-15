4553aa2b95b983acf6d54dca6d7c6d816ac9fba6
Bogdan Petrukovich <viorbrint@gmail.com>
2025-01-16 00:44:13 +0300

    feat: catch sf error

--- Components/Pages/UserPage/SalesforceFormDialog.razor.cs
+++ Components/Pages/UserPage/SalesforceFormDialog.razor.cs
@@ -6,20 +6,35 @@ namespace Forms.Components.Pages.UserPage;
 
 public partial class SalesforceFormDialog : ComponentBase
 {
-    [Inject] private SalesforceService SalesforceService { get; set; } = null!;
-    [CascadingParameter] private MudDialogInstance Dialog { get; set; } = null!;
-    private bool IsValid {get; set;}
+    [Inject]
+    private SalesforceService SalesforceService { get; set; } = null!;
+
+    [Inject]
+    private SnackbarFacade SnackbarFacade { get; set; } = null!;
+
+    [CascadingParameter]
+    private MudDialogInstance Dialog { get; set; } = null!;
+    private bool IsValid { get; set; }
     private UserModel UserModel { get; } = new();
-    
+
     private async Task SubmitToSalesforce()
     {
-        await SalesforceService.CreateAccountWithContact(
-            UserModel.AccountName,
-            UserModel.FirstName,
-            UserModel.LastName,
-            UserModel.Email
-        );
-        Dialog.Close(DialogResult.Ok(true));
+        try
+        {
+            await SalesforceService.CreateAccountWithContact(
+                UserModel.AccountName,
+                UserModel.FirstName,
+                UserModel.LastName,
+                UserModel.Email
+            );
+            Dialog.Close(DialogResult.Ok(true));
+            SnackbarFacade.Success("Successfully submitted to Salesforce");
+        }
+        catch (Exception ex)
+        {
+            Dialog.Close(ex);
+            SnackbarFacade.Error("Error submitting to Salesforce");
+        }
     }
 
     private void Cancel()
@@ -27,6 +42,7 @@ public partial class SalesforceFormDialog : ComponentBase
         Dialog.Cancel();
     }
 }
+
 public class UserModel
 {
     public string AccountName { get; set; } = string.Empty;
